21ce7bff9eb0d30cbdfb9f95c16221c6899efe1c
cherryrenee <sujinn1205@naver.com>
2025-05-09 14:38:34 +0900

    sf
    sf

--- index.mjs
+++ index.mjs
@@ -3,3 +3,12 @@ import { singerData } from "./data.mjs";
 import { programData } from "./data.mjs";
 import { actorData } from "./data.mjs";
 import { capitalData } from "./data.mjs";
+
+const startBtn = document.querySelector(".start-btn");
+const mainPage = document.querySelector(".mainPage");
+const quizPage = document.querySelector(".quiz-page");
+
+startBtn.addEventListener("click", () => {
+  mainPage.style.display = "none";
+  quizPage.style.dispaly = "flex";
+});
