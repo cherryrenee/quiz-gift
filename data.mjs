50d3e332e6b42c2725870b08c7fa37a373b5a6f6
cherryrenee <sujinn1205@naver.com>
2025-05-09 14:25:17 +0900

    f
    ff

---
+++ data.mjs
@@ -0,0 +1,5 @@
+export landmarkData = ["피라미드", "자유의 여신상", "에버랜드", "피사의 사탑"];
+export singerData = ["태진아", "박지현", "태연", "영탁"];
+export programData = ["불타는 트롯맨", "내 딸 금사월","내 딸 서영이","나왔다! 장보리"];
+export actorData = ["진성","김용건","이금복","양미경",];
+export capitalData = ["시드니","마닐라","마드리드","로마"];
