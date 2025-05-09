a8332516330761fbeea012641e870dff2f28442e
cherryrenee <sujinn1205@naver.com>
2025-05-09 14:34:04 +0900

    se
    se

--- data.mjs
+++ data.mjs
@@ -1,5 +1,15 @@
-export landmarkData = ["피라미드", "자유의 여신상", "에버랜드", "피사의 사탑"];
-export singerData = ["태진아", "박지현", "태연", "영탁"];
-export programData = ["불타는 트롯맨", "내 딸 금사월","내 딸 서영이","나왔다! 장보리"];
-export actorData = ["진성","김용건","이금복","양미경",];
-export capitalData = ["시드니","마닐라","마드리드","로마"];
+export const landmarkData = [
+  "피라미드",
+  "자유의 여신상",
+  "에버랜드",
+  "피사의 사탑",
+];
+export const singerData = ["태진아", "박지현", "태연", "영탁"];
+export const programData = [
+  "불타는 트롯맨",
+  "내 딸 금사월",
+  "내 딸 서영이",
+  "나왔다! 장보리",
+];
+export const actorData = ["진성", "김용건", "이금복", "양미경"];
+export const capitalData = ["시드니", "마닐라", "마드리드", "로마"];
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import { landmarkData } from "./data.mjs";
+import { singerData } from "./data.mjs";
+import { programData } from "./data.mjs";
+import { actorData } from "./data.mjs";
+import { capitalData } from "./data.mjs";
